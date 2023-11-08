50835c4b0ea3a65be23b4621cfeb84f05949cd4d
Munjiwon <wldnjsdl269@gmail.com>
2023-11-08 09:54:27 +0000

    test

---
+++ chap7/prob3/file_rock.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv) {
+   static struct flock lock;
+   int fd, ret, c;
+   if (argc < 2) {
+      fprintf(stderr, "How to use : %s File \n", argv[0]);
+      exit(1);
+   }
+   fd = open(argv[1], O_WRONLY);
+   if (fd == -1) {
+      printf("Failed to open file\n");
+      exit(1);
+   }
+   lock.l_type = F_WRLCK;
+   lock.l_start = 0;
+   lock.l_whence = SEEK_SET;
+   lock.l_len = 0;
+   lock.l_pid = getpid();
+   ret = fcntl(fd, F_SETLKW, &lock);
+   if(ret == 0) { 
+      c = getchar();
+   }
+   printf("Succese to lock file");
+}
